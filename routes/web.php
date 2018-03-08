ec9b07fe8a34bb4bd6d380a6fd754ea96b0e8b21
vhkdiy <404924175@qq.com>
2018-03-08 11:03:59 +0800

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
